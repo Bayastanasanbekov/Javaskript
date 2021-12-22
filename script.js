1ae98b44a91efec7af62925b0bf83aa6a6c0a69c
Bayastanasnbekov <baiaskoas07@gmail.com>
2021-12-22 14:27:06 +0600

    add var

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 // alert ("Hello world")
 
+// coonsole.log("Hello")
+var firstName;
+firstName = "Vasya";
+console.log(firstName);
+ 
+
+// example 1 math 
+var potatoes = 5, tomatoes = 3;
+var vegetables = potatoes + tomatoes;
+var result = vegetables / 2;
+
+// example 2 math 
+result - (2 + 1) / 3 - 6 
 
 
 
+// home example Math/FITH
+ var book = 2, copybook = 4;
+var 
 
 
-
-
-
+var example = 10 === 10; //equales
+var
 
 
 
