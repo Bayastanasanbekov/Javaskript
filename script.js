711bfffeeb496b99ee127f103f0f6128c5409ece
Bayastanasnbekov <baiaskoas07@gmail.com>
2021-12-24 15:02:12 +0600

    commit

--- script.js
+++ script.js
@@ -1,30 +1,85 @@
-// alert ("Hello world")
+// // var example1 = 10 === 10; //equals
+// // var example2 = 11 > 10; // more than
+// // var example3 = 10 < 11; //less than
+// // var example4 = 10 >= 10; //more than or equal
+// // var example5 = 10 <= 10; //less than or equal
+// // var example6 = 10 !== 10; //not equals 
 
-// coonsole.log("Hello")
-var firstName;
-firstName = "Vasya";
-console.log(firstName);
+
+// // var a = 5;
+// // var b = 10;
+// // var exmaple7 = a < b ;
+
+
+// // var c = 12;
+// // var d = 11;
+// // if (c < d) { //true
+// //   alert("C is less than D");
+// //   console.log ('Hello world')
+// // }
+
+
+// // var e = 3.14;
+// // if (e !== 3.14 ) {
+// //   alert("Why don't you know numver PI");
+// // }
+// // else {
+// //   alert("hi bye!")
+// // }
+
+// // var age = 10 ;
+// // if (age >= 18) {
+// //   alert("do yuo have a car"); //true
+// // }
+// // else {
+// //   alert("Erlan Bayke are stupi!"); //false
+// // }
+
+
+// // var apple = 6;
+// // var pear = 4;
+
+// // //}
+// //
+
+
+
+
+// // var username = prompt ("What is your name?");
+// // alert("Welcome " + username + " good, cute person");
+
+
+
+// // var username = prompt ("What is your before live?");
+// // alert("Welcome the new living")
+// // //}
+
+
+// // var username = prompt ("What is your study school?");
+// // alert("Welcome the new school")
+
+// // var username = prompt ("How old are you");
+// // alert("Welcome people age " + username )
+
+// // var username = prompt ("How are you");
+// // alert("Molodes " + username  );
+
+// // var message = prompt("What")
+
+// var year = prompt("What year Kyrgyztan gained independence?");
+// if (year == 1991 ){
+//     alert("Correct ")
+// }
+// else {
+//     alert("Incorrect")
+// }
+
+// var username = prompt ("What is 150+150=?");
  
+//      alert( " Отсоси у тракториста")
 
-// example 1 math 
-var potatoes = 5, tomatoes = 3;
-var vegetables = potatoes + tomatoes;
-var result = vegetables / 2;
-
-// example 2 math 
-result - (2 + 1) / 3 - 6 
-
-
-
-// home example Math/FITH
- var book = 2, copybook = 4;
-var 
-
-
-var example = 10 === 10; //equales
-var
-
-
+var city = prompt("В каком городу ты живёш")
+if(city) 
 
 
 
